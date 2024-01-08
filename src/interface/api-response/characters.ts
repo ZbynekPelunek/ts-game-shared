e994ce362680d8fe3f1c54a330eb4692c809dc39
Zbynek Pelunek <zbynekpelunek@gmail.com>
2024-01-08 02:05:29 +0100

    add API response Get All Characters

--- src/interface/api-response/characters.ts
+++ src/interface/api-response/characters.ts
@@ -2,6 +2,10 @@ import { IAdventure } from '../adventures.interface';
 import { CharacterFrontend } from '../character/characters.interface';
 import { Common_Response_Error, Common_Response_Success } from './common';
 
+export interface Characters_GET_All extends Common_Response_Success {
+  characters: CharacterFrontend[]
+}
+
 export interface Characters_GET_one extends Common_Response_Success {
   character: CharacterFrontend
 }
@@ -24,5 +28,6 @@ export interface POST_characterActions {
   character: CharacterFrontend
 }
 
+export type Response_Characters_GET_All = Characters_GET_All | Common_Response_Error;
 export type Response_Characters_GET_one = Characters_GET_one | Common_Response_Error;
 export type Response_Characters_POST = Characters_POST | Common_Response_Error;
--- src/interface/item.interface.ts
+++ src/interface/item.interface.ts
@@ -1,5 +1,3 @@
-import { Types } from 'mongoose';
-
 export interface CommonItemParams {
   itemId: number;
   name: string;
