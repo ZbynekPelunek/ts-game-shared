57537317f35966867b337021e74e04538affb6fa
Zbynek Pelunek <zbynekpelunek@gmail.com>
2025-09-23 01:03:05 +0200

    add item internal interface response

--- src/interface/item/apiResponse.ts
+++ src/interface/item/apiResponse.ts
@@ -9,9 +9,19 @@ export interface ListItems extends CommonSuccessResponse {
   items: CommonItemParams[] | CommonItemsEquipmentParams[];
 }
 
+export interface ListItemsInternal {
+  items: CommonItemParams[] | CommonItemsEquipmentParams[];
+}
+
 export interface GetItem extends CommonSuccessResponse {
   item: CommonItemParams | CommonItemsEquipmentParams;
 }
 
+export interface GetItemInternal {
+  item: CommonItemParams | CommonItemsEquipmentParams;
+}
+
 export type ListItemsResponse = ListItems | CommonErrorResponse;
+export type ListItemsInternalResponse = ListItemsInternal | CommonErrorResponse;
 export type GetItemResponse = GetItem | CommonErrorResponse;
+export type GetItemInternalResponse = GetItemInternal | CommonErrorResponse;
